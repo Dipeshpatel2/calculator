95ba20d4689288a6d60220fb54930407a1e4abe5
Dipeshpatel2e <dipeshpatel2@yahoo.co.uk>
2023-03-31 14:58:46 +0100

    classes added to buttons on HTML code

---
+++ app.js
@@ -0,0 +1,11 @@
+// variables created and assigned to classes made in HTML. classes found in brackets.
+//querySelector for single class and querySelectorAll for multiple elements of the same class  
+const screen = document.querySelector(".screen")
+const allClear = document.querySelector(".clear")
+const numbers = document.querySelectorAll(".number")
+const operators = document.querySelectorAll(".operator")
+
+//when the buttons are pressed, declaring them as variables temporary stores them
+let firstNumber = ""
+let operator = ""
+let secondNubmer = ""
