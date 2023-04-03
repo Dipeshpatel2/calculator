b48c9538a4b9d4782df62ece46bf49296981f1b4
Dipeshpatel2e <dipeshpatel2@yahoo.co.uk>
2023-04-03 15:40:31 +0100

    handleNumber Click function added

--- app.js
+++ app.js
@@ -2,10 +2,12 @@
 //querySelector for single class and querySelectorAll for multiple elements of the same class  
 // setting them as const numbers for eg-makes all the buttons attached to .number class into an array
 const screen = document.querySelector(".screen")
+const topScreen = document.querySelector(".top-screen")
 const allClear = document.querySelector(".clear")
 const numbers = document.querySelectorAll(".number")
 const operators = document.querySelectorAll(".operator")
 const deletes = document.querySelector(".delete")
+const equal = document.querySelector(".equal")
 
 //when the buttons are pressed, declaring them as variables temporary stores them
 let firstNumber = ""
@@ -14,7 +16,40 @@ let secondNubmer = ""
 
 // creating this function means that everytime a button is pressed it will show in the console
 const handleNumberClick = (event) =>{
-    console.log(event.target.innerText)
+    if (screen.innerText == "+"){
+        topScreen.innerText += " + "
+        screen.innerText= ""
+    } else if (screen.innerText == "-"){
+        topScreen.innerText += " - "
+        screen.innerText= ""
+    } else if (screen.innerText == "/"){
+        topScreen.innerText += " / "
+        screen.innerText= ""
+    }else if (screen.innerText == "*"){
+        topScreen.innerText += " * "
+        screen.innerText= ""    
+    }
+    const innerText = parseInt(event.target.innerText)
+    //screen.innerText = screen.innerText+innerText
+    screen.innerText += innerText
+}
+
+const handleOperatorClick = (event) =>{
+    const innerText = (event.target.innerText)
+    operator = (innerText)
+    firstNumber = screen.innerText
+    console.log(firstNumber)
+    topScreen.innerText = firstNumber
+    screen.innerText = operator
+}
+
+const handleClearClick = () =>{
+    screen.innerText = ""
+    topScreen.innerText = ""
+}
+
+const handleCalculate = (firstNumb, oper, secondNumb) => {if oper === 
+
 }
 //
 //loop over numbers array
@@ -25,5 +60,7 @@ numbers.forEach((number) => {
 })
 
 operators.forEach((operator) => {
-    operator.addEventListener("click", handleNumberClick)
+    operator.addEventListener("click", handleOperatorClick)
 })
+
+allClear.addEventListener("click", handleClearClick)
